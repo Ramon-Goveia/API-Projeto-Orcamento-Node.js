afc5417a67737578f453728e88c4002b0ee0b02b
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-12-08 12:46:22 -0300

    nodemailer config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const nodemailer = require('nodemailer')
 
 require('./models/Orcamento')
 const Orcamento = mongoose.model('Orcamento')
@@ -25,10 +26,39 @@ app.post('/orcamento', async (req, res) => {
             message: "ERRO: Solicitação de orçamento não enviada!"
         })
     })
-    return res.json({
-        error: false,
-        message: "Solicitação de orçamento enviada com sucesso!"
+    var transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+            user: "cffe5c8201e7fb",
+            pass: "11dc8345284943"
+        },
     })
+
+    var emailHtml = 'Prezado(a)<br><br> Recebemos a sua solicitação.<br><br>Em breve será encaminhado o orçamento.<br><br>';
+
+    var eamilTexto = 'Prezado(a)\n\nRecebemos a sua solicitação.\n\nEm breve será encaminhado o orçamento.\n\n';
+
+    emailSendInfo = {
+        from: '391b1a8705-813402@inbox.mailtrap.io',
+        to: req.body.email,
+        subject: "Recebemos a solicitação de orçamento",
+        text: eamilTexto,
+        html: emailHtml,
+    }
+
+    await transport.sendMail(emailSendInfo, function(err){
+        if(err) return res.status(400).json({
+            error: true,
+            message: "ERRO: Solicitação de orçamento não enviada!"
+        })
+        
+        return res.json({
+            error: false,
+            message: "Solicitação de orçamento enviada com sucesso!"
+        })
+    })
+   
 })
 
 app.listen(8080, () => {
